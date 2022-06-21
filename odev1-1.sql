d87fbfd95aa3ba111d6c97ce4c62030673116491
sabanusta <susta38@gmail.com>
2022-06-21 13:24:10 +0300

    Create odev1-1.sql

---
+++ odev1-1.sql
@@ -0,0 +1,4 @@
+SELECT title, description, FROM film;
+SELECT title FROM film; where  film_length > 60 and film_length <75;
+SELECT title FROM film; where  rental_rate = 0.99  and  replacement_cost = 12.99 or  replacement_cost=28.99
+SELECT TITLE FROM CUSTOMER  WHERE  first_name= 'mary' 
